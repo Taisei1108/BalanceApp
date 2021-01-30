faec3100d0b53f14459b28cc6359a2f9af00f78f
Taisei1108 <taikitigw7@gmail.com>
2021-01-30 10:37:21 +0900

    localsettingの設定

---
+++ src/BalanceProject/local_setting.py
@@ -0,0 +1,1 @@
+SECRET_KEY = '@_ivz_(9*#bo%i9^nrf3xr!fqd7sk)8l7jj2nh2=9bfu+b3^o#'
--- src/BalanceProject/settings.py
+++ src/BalanceProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@_ivz_(9*#bo%i9^nrf3xr!fqd7sk)8l7jj2nh2=9bfu+b3^o#'
+#xxxxxxxx
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -118,3 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
